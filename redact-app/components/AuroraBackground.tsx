fa717b7cfaf9dcd677aa1d79e28c73dad93a9781
anushk3984 <anushk3984@gmail.com>
2024-11-06 18:35:25 +0530

    added flip word

--- redact-app/components/AuroraBackground.tsx
+++ redact-app/components/AuroraBackground.tsx
@@ -3,8 +3,10 @@
 import { motion } from "framer-motion";
 import React from "react";
 import { AuroraBackground } from "./ui/aurora-background";
+import { FlipWords } from "./ui/flip-words";
 
 export function AuroraBackgroundDemo() {
+    const words =["PDFs", "Images", "Videos", "Text","Document"];
     return (
         <AuroraBackground>
             <motion.div
@@ -18,13 +20,14 @@ export function AuroraBackgroundDemo() {
                 className="relative flex flex-col gap-4 items-center justify-center px-4"
             >
                 <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-                    Background lights are cool you know.
+                    Secure your <FlipWords words={words} /><br/>
+                    with REDACT
                 </div>
                 <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-                    And this, is chemical burn.
+                    Powerful Redaction Tool
                 </div>
                 <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-                    Debug now
+                    Getting Started
                 </button>
             </motion.div>
         </AuroraBackground>
