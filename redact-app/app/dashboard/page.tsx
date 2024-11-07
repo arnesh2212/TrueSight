e0795a512c252ed61d1b15078aa802b36df58215
anushk3984 <anushk3984@gmail.com>
2024-11-07 18:50:01 +0530

    added floating dock

--- redact-app/app/dashboard/page.tsx
+++ redact-app/app/dashboard/page.tsx
@@ -1,15 +1,86 @@
-import { FloatingDock } from "@/components/ui/floating-dock";
+"use client";
 import { FileUploadDemo } from "@/components/FileUpload";
 import { FileUpload } from "@/components/ui/file-upload";
-import { FloatingDockDemo } from "@/components/dock";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import {
+  IconBrandGithub,
+  IconBrandX,
+  IconExchange,
+  IconHome,
+  IconNewSection,
+  IconTerminal2,
+} from "@tabler/icons-react";
+
 
 
 export default function Home() {
-    return (
-        <main className="flex justify-center items-center min-h-screen bg-gray-200">
-            <FileUploadDemo>
-            </FileUploadDemo>
-                    
-        </main>
-    );
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+
+    {
+      title: "Products",
+      icon: (
+        <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Components",
+      icon: (
+        <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Aceternity UI",
+      icon: (
+        <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Changelog",
+      icon: (
+        <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+
+    {
+      title: "Twitter",
+      icon: (
+        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "GitHub",
+      icon: (
+        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+  ];
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-200">
+      <FileUploadDemo>
+      </FileUploadDemo>
+      <div className="flex items-center justify-center h-[35rem] w-full">
+    <FloatingDock
+      mobileClassName="translate-y-20" // only for demo, remove for production
+      items={links}
+    />
+  </div>
+      
+
+    </div>
+    
+    
+  );
 }
