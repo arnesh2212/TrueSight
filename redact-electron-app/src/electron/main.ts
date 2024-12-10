0aabebc2660168609f208ca371ff20591e651444
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-12-11 00:02:04 +0530

    added favicons

--- redact-electron-app/src/electron/main.ts
+++ redact-electron-app/src/electron/main.ts
@@ -4,6 +4,8 @@ import path from "path";
 type test = String;
 
 app.on("ready", () => {
-	const mainWindow = new BrowserWindow({});
+	const mainWindow = new BrowserWindow({
+		icon: path.join(__dirname, "public", "favicon.ico"),
+	});
 	mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
 });
