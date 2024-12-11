3112d046ae3d77d383080233ecdce6a8fec933ca
anushk3984 <anushk3984@gmail.com>
2024-12-11 22:15:55 +0530

    added icons

--- redact-app/app/(dashboard)/text/page.tsx
+++ redact-app/app/(dashboard)/text/page.tsx
@@ -1,7 +1,66 @@
 "use client";
+import * as React from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+
+const top100Films = [
+	{ title: 'The Shawshank Redemption', year: 1994 },
+	{ title: 'The Godfather', year: 1972 },
+	{ title: 'The Godfather: Part II', year: 1974 },
+	{ title: 'The Dark Knight', year: 2008 },
+	{ title: '12 Angry Men', year: 1957 },
+	{ title: "Schindler's List", year: 1993 },
+	{ title: 'Pulp Fiction', year: 1994 },
+	{
+		title: 'The Lord of the Rings: The Return of the King',
+		year: 2003,
+	},
+	{ title: 'The Good, the Bad and the Ugly', year: 1966 },
+	{ title: 'Fight Club', year: 1999 },
+	{
+		title: 'The Lord of the Rings: The Fellowship of the Ring',
+		year: 2001,
+	},
+	{
+		title: 'Star Wars: Episode V - The Empire Strikes Back',
+		year: 1980,
+	},
+	{ title: 'Forrest Gump', year: 1994 },
+	{ title: 'Inception', year: 2010 },
+	{
+		title: 'The Lord of the Rings: The Two Towers',
+		year: 2002,
+	},
+	{ title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+	{ title: 'Goodfellas', year: 1990 },
+	{ title: 'The Matrix', year: 1999 },
+	{ title: 'Seven Samurai', year: 1954 },
+	{
+		title: 'Star Wars: Episode IV - A New Hope',
+		year: 1977,
+	},
+	{ title: 'City of God', year: 2002 },
+	{ title: 'Se7en', year: 1995 },
+	{ title: 'The Silence of the Lambs', year: 1991 },
+	{ title: "It's a Wonderful Life", year: 1946 },
+	{ title: 'Life Is Beautiful', year: 1997 },
+	{ title: 'The Usual Suspects', year: 1995 },
+	{ title: 'Léon: The Professional', year: 1994 },
+	{ title: 'Spirited Away', year: 2001 },
+	{ title: 'Saving Private Ryan', year: 1998 },
+	{ title: 'Once Upon a Time in the West', year: 1968 },
+	{ title: 'American History X', year: 1998 },
+	{ title: 'Interstellar', year: 2014 },
+];
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
 
 import { useState } from "react";
-import { Textarea } from "@/components/ui/textarea";
+// import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import {
 	Select,
@@ -12,8 +71,8 @@ import {
 	SelectValue,
 } from "@/components/ui/select";
 import { Slider } from "@/components/ui/slider";
-import { Separator } from "@/components/ui/separator";
-import { IoInformationCircleOutline } from "react-icons/io5";
+// import { Separator } from "@/components/ui/separator";
+// import { IoInformationCircleOutline } from "react-icons/io5";
 import {
 	HoverCard,
 	HoverCardContent,
@@ -246,8 +305,36 @@ export default function Home() {
 								</HoverCard>
 							</AccordionTrigger>
 							<AccordionContent>
-								Yes. It comes with default styles that matches the other
-								components&apos; aesthetic.
+								{/* Yes. It comes with default styles that matches the other
+								components&apos; aesthetic. */}
+
+								<Autocomplete
+									multiple
+									id="checkboxes-tags-demo"
+									options={top100Films}
+									disableCloseOnSelect
+									getOptionLabel={(option) => option.title}
+									renderOption={(props, option, { selected }) => {
+										const { key, ...optionProps } = props;
+										return (
+											<li key={key} {...optionProps}>
+												<Checkbox
+													icon={icon}
+													checkedIcon={checkedIcon}
+													style={{ marginRight: 8 }}
+													checked={selected}
+												/>
+												{option.title}
+											</li>
+										);
+									}}
+									style={{ width: 500 }}
+									renderInput={(params) => (
+										<TextField {...params} label="Checkboxes" placeholder="Favorites" />
+									)}
+								/>
+
+
 							</AccordionContent>
 						</AccordionItem>
 						<AccordionItem value="item-3">
